de7637ad37abe9313d40a7dd163b706c60b91e77
Andrej Jocic <andrej.jocic.24@gmail.com>
2023-08-31 17:08:21 +0200

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+// Transforming image URL
+// FROM: https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+// TO: https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+
+function getCroppedImageUrl(url: string) {
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
+
+export default getCroppedImageUrl;
