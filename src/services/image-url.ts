9aefe6b28cc08e4499b5a6ad64e26aaa6ec52e23
Andrej Jocic <andrej.jocic.24@gmail.com>
2023-09-02 21:29:32 +0200

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -2,9 +2,11 @@
 // FROM: https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
 // TO: https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
 
+import noImage from '../assets/no-image-placeholder.webp';
+
 function getCroppedImageUrl(url: string) {
   // TODO: add an 'No cover' image if game doesn't have one
-  if (!url) return '';
+  if (!url) return noImage;
 
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
