0a3249a5755fb63abdbb1c8a689438d4399c0cde
Andrej Jocic <andrej.jocic.24@gmail.com>
2023-09-15 17:58:32 +0200

    Setup routing

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Oops...</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page doesn't exist"
+            : 'An unexpected error has occurred.'}
+        </Text>
+      </Box>
+    </>
+  );
+}
+
+export default ErrorPage;
