7e477b2e9682cd93403ede956afcbf707954a89f
Andrej Jocic <andrej.jocic.24@gmail.com>
2023-09-16 16:44:12 +0200

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import ExpendableText from '../components/ExpendableText';
 import GameAttributes from '../components/GameAttributes';
 import useGame from '../hooks/useGame';
+import GameTrailer from '../components/GameTrailer';
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -17,6 +18,7 @@ function GameDetailPage() {
       <Heading>{game.name}</Heading>
       <ExpendableText>{game.description_raw}</ExpendableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 }
